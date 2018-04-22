dbcdc63c0dc8820f576a614e402ba53181a9b2ea
EchoWhisky <echowhisky@users.noreply.github.com>
2018-04-22 15:04:45 +0100

    changes to this python file to fix HOME path
    this had the /root directory hard-coded as the home path
    its been changed now to try to use the HOME environment variable
    to determine the right path location

--- config/jupyter/jupyter_notebook_config.py
+++ config/jupyter/jupyter_notebook_config.py
@@ -1,7 +1,9 @@
 import json
 import os
 import sys
-sys.path.append('/root/.jupyter/extensions/')
+
+home = os.environ['HOME']
+sys.path.append('{0}/.jupyter/extensions/'.format(home))
 
 c.JupyterApp.ip = '*'
 c.JupyterApp.port = 80
@@ -9,8 +11,8 @@ c.JupyterApp.open_browser = False
 c.JupyterApp.allow_credentials = True
 c.JupyterApp.nbserver_extensions = ['jupyter_nbgallery.status', 'jupyter_nbgallery.post']
 c.JupyterApp.reraise_server_extension_failures = True
-c.JupyterApp.extra_static_paths = ['/root/.jupyter/static']
-c.JupyterApp.extra_nbextensions_path = ['/root/.jupyter/extensions/']
+c.JupyterApp.extra_static_paths = ['{0}/.jupyter/static'.format(home)]
+c.JupyterApp.extra_nbextensions_path = ['{0}/.jupyter/extensions/'.format(home)]
 c.JupyterApp.tornado_settings = {'static_url_prefix': '/Jupyter/static/'}
 c.JupyterApp.allow_origin = 'https://nb.gallery'
 
@@ -23,10 +25,10 @@ for var in [x for x in os.environ if x.startswith(config_prefix)]:
   c.JupyterApp[var[len(config_prefix):].lower()] = os.environ[var]
 
 def load_config():
-  return json.loads(open('/root/.jupyter/nbconfig/common.json').read())
+  return json.loads(open('{0}/.jupyter/nbconfig/common.json'.format(home).read())
 
 def save_config(config):
-  with open('/root/.jupyter/nbconfig/common.json', 'w') as output:
+  with open('{0}/.jupyter/nbconfig/common.json'.format(home), 'w') as output:
     output.write(json.dumps(config, indent=2))
 
 # Override gallery location
