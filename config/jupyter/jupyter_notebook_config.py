436d37fb3c7d9ecc2f32e4f606f70f2418115375
EchoWhisky <17090642+echowhisky@users.noreply.github.com>
2019-03-16 09:43:59 +0000

    aligning jupyter_notebook_config.py better w/ other
    trying to get this file in sync  with updates in the Alpine version,
    but in a compatible way. will probably make a PR for the Alpine version
    so that these files can be the same

--- config/jupyter/jupyter_notebook_config.py
+++ config/jupyter/jupyter_notebook_config.py
@@ -3,39 +3,44 @@ import os
 import sys
 
 home = os.environ['HOME']
-sys.path.append('{0}/.jupyter/extensions/'.format(home))
+jupyterPrefs_dir = '{0}/.jupyter/'.format(home)
+jP_static = jupyterPrefs_dir + 'static'
+jP_extensions = jupyterPrefs_dir + 'extensions/'
+jP_nbconfig_json = jupyterPrefs_dir + 'nbconfig/common.json'
 
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
-c.NotebookApp.allow_credentials = True
-c.NotebookApp.nbserver_extensions = {'jupyter_nbgallery.status':True, 'jupyter_nbgallery.post':True}
-c.NotebookApp.reraise_server_extension_failures = True
-c.NotebookApp.extra_static_paths = ['{0}/.jupyter/static'.format(home)]
-c.NotebookApp.extra_nbextensions_path = ['{0}/.jupyter/extensions/'.format(home)]
-c.NotebookApp.tornado_settings = {'static_url_prefix': '/Jupyter/static/'}
-c.NotebookApp.allow_origin = 'https://nb.gallery'
+sys.path.append(jupyterPrefs_dir)
+
+c.JupyterApp.ip = '0.0.0.0'
+c.JupyterApp.port = 8888
+c.JupyterApp.open_browser = False
+c.JupyterApp.allow_credentials = True
+c.JupyterApp.nbserver_extensions = ['jupyter_nbgallery.status', 'jupyter_nbgallery.post']
+c.JupyterApp.reraise_server_extension_failures = True
+c.JupyterApp.extra_static_paths = [jP_static]
+c.JupyterApp.extra_nbextensions_path = [jP_extensions]
+c.JupyterApp.tornado_settings = {'static_url_prefix': '/Jupyter/static/'}
+c.JupyterApp.allow_origin = 'https://localhost:3000'
 
 # needed to receive notebooks from the gallery
-c.NotebookApp.disable_check_xsrf = True
+c.JupyterApp.disable_check_xsrf = True
 
 # Update config from environment
 config_prefix = 'NBGALLERY_CONFIG_'
 for var in [x for x in os.environ if x.startswith(config_prefix)]:
-  c.NotebookApp[var[len(config_prefix):].lower()] = os.environ[var]
+  c.JupyterApp[var[len(config_prefix):].lower()] = os.environ[var]
 
 def load_config():
-  return json.loads(open('{0}/.jupyter/nbconfig/common.json'.format(home)).read())
+  return json.loads(open(jP_nbconfig_json).read())
 
 def save_config(config):
-  with open('{0}/.jupyter/nbconfig/common.json'.format(home), 'w') as output:
+  with open(jP_nbconfig_json) as output:
     output.write(json.dumps(config, indent=2))
 
 # Override gallery location
 nbgallery_url = os.getenv('NBGALLERY_URL')
 if nbgallery_url:
   print('Setting nbgallery url to %s' % nbgallery_url)
-  c.NotebookApp.allow_origin = nbgallery_url
+  c.JupyterApp.allow_origin = nbgallery_url
   config = load_config()
   config['nbgallery']['url'] = nbgallery_url
   save_config(config)
